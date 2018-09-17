2f1982fd124b123cfb611673ea3a366966a64548
Mustafa Tunc <mtunc@hotmail.com>
2018-09-17 02:54:02 -0500

    done

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,5 @@
 
-    // START CODING BELOW!!
-
+  
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBcb6tIJjPK_G5pe1V_mGEwMpzbVl5FNOk",
@@ -16,7 +15,8 @@
     // Create a variable to reference the database.
     var scheduleTable = firebase.database();
 
-    // Initial Values
+    // Set up the Initial static Variables
+
     var name = "";
     var destination= "";
     var firstTrain = "";
@@ -24,11 +24,11 @@
     
    
 
-    // Capture Button Click
+    // Capture Button Click 
     $("#add-train").on("click", function(event) {
       event.preventDefault();
 
-      // Grabbed values from text-boxes
+      // Grab values from text-boxes store in the variables
       name = $("#name-input").val().trim();
       destination = $("#destination-input").val().trim();
       firstTrain= moment($("#time-input").val().trim(),"HH:mm").subtract(10, "years").format("X");
@@ -36,7 +36,7 @@
       
     
 
-      // Code for "Setting values in the database"
+      // push the  values into the firebase database"
        scheduleTable.ref("/trains").push({
         name: name,
         destination: destination,
@@ -58,27 +58,31 @@ return false;
 
     });
 
-    // Firebase watcher + initial loader HINT: .on("value")
+    // Firebase stores the new information as snapshots 
+    
     scheduleTable.ref("/trains").on("child_added", function(childSnapshot) {
 
         let sv=childSnapshot.val();
 
 
-let trainName = sv.name;
-let trainDestination = sv.destination;
-let trainFrequency = sv.frequency;
-let FirstTrain = sv.firstTrain;
+        let trainName = sv.name;
+        let trainDestination = sv.destination;
+        let trainFrequency = sv.frequency;
+        let FirstTrain = sv.firstTrain;
 
 
-//calculates the how many minutes left
+//calculates the minutes left
       
     let Remainder = moment().diff(moment.unix(FirstTrain), "minutes") % trainFrequency;
     let minutesAway = trainFrequency - Remainder;
 
-    // To calculate the arrival time, add the tMinutes to the currrent time
+
+    // calculates the next arrival time
+
     let nextArrival = moment().add(minutesAway, "m").format("hh:mm A");
 
-            
+       
+    // Adds the new information to the page
 
 $("#trainSchedule > tbody").append("<tr><td>" + trainName+ "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "mins" + "</td></tr>");
 
